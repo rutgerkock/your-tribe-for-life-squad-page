ffb92c8fd9cecb5003ebf1e787a568656d3a548a
TomDeeterink1 <deeterinktom@gmail.com>
2024-09-04 16:22:22 +0200

    Full svelte push
    Svelte push with the right connection to directus

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<h1>baguette</h1>
+<script>
+    /** @type {import('./$types').PageData} */
+    export let data;
+    
+    // Check if the data has been received and is an array
+    console.log("Received data in +page.svelte:", data);
+</script>
+ 
+<!-- Only render if we have people in the data -->
+{#if data.people}
+    {#each data.people as person}
+        <img src="{person.avatar}" width="150px" height="150px">
+        <h1>{person.name}</h1>
+        <p>{person.bio}</p>
+    {/each}
+{:else}
+    <!-- This will show if no people are available -->
+    <p>No data available</p>
+{/if}
+ 
