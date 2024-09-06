6a828eb368412d61c1570cf5baa2dab8148412f3
Shanna van der Werve <shanna.van.der.werve@hva.nl>
2024-09-06 10:23:36 +0200

    Squad filter done

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,9 +1,10 @@
  /** @type {import('./$types').PageLoad} */
 import getDirectusInstance from '$lib/directus';
 import { readItems } from '@directus/sdk';
+
 export async function load({ fetch }) {
     const directus = getDirectusInstance(fetch);
     return {
-        people: await directus.request(readItems('person')),
-    };
+        people: await directus.request(readItems('person'))
+    }
 }
--- src/routes/squad/[squad]/+page.svelte
+++ src/routes/squad/[squad]/+page.svelte
@@ -5,6 +5,16 @@
     // Check if the data has been received and is an array
     console.log("Received data in +page.svelte:", data);
 </script>
+<style>
+    @import '/styles/main.css';
+</style>
+
+<div class="squads">
+    <a href="/">Alle squads</a>
+    <a href="/squad/3">Squad D</a>
+    <a href="/squad/4">Squad E</a>
+    <a href="/squad/5">Squad F</a>
+</div>
  
 <!-- Only render if we have people in the data -->
 {#if data.people}
