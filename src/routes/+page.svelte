1ace10cffaed1bf18d166a63478fb9bfec2e7cdc
Anna-Kyra <anna-kyra.strik@hva.nl>
2024-09-06 11:34:11 +0200

    Flip cards basis
    - html basis cards
    - CSS flip card (tutorial: https://www.codeguage.com/blog/flip-card-css)
    
    volgende stap:
    - Interactief klikbaar met js
    - design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,13 +8,66 @@
  
 <!-- Only render if we have people in the data -->
 {#if data.people}
-    {#each data.people as person}
-        <img src="{person.avatar}" width="150px" height="150px">
-        <h1>{person.name}</h1>
-        <p>{person.bio}</p>
-    {/each}
+    <ul>
+        {#each data.people as person}
+        <li>
+            <article class="card-front">
+                <img src="{person.avatar}" width="150px" height="150px" alt=''>
+                <h1>{person.name}</h1>
+                
+            </article>
+            <article class="card-back">
+                <p>{person.bio}</p>
+                <p>Insert profilecard link</p>
+            </article>
+        </li>
+        {/each}
+    </ul>
 {:else}
     <!-- This will show if no people are available -->
     <p>No data available</p>
 {/if}
  
+<style>
+    ul {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 1em;
+    }
+    li {
+        height: 25em;
+        width: 15em;
+        background-color: goldenrod;
+        border-radius: .5em;
+        list-style: none;
+        position: relative;
+        transform-style: preserve-3d;
+        cursor: pointer;
+
+        /* transform: rotateY(180deg); */
+    }
+
+    article {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        backface-visibility: hidden;
+        
+        
+    }
+
+    .card-front {
+        display:grid; 
+        place-content:center;
+        text-align:center;
+        z-index: 1;
+        background-color: coral;
+    }
+
+    .card-back {
+        background-color: cadetblue;
+        transform: rotateY(180deg);
+        
+    }
+    
+</style>
