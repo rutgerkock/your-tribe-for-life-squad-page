16a80d06999bfea7ae649f56a8a14c030fea368a
rutgerkock <114153177+rutgerkock@users.noreply.github.com>
2024-09-06 15:38:00 +0200

    test branch

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,7 +17,7 @@
     <span>.</span>
     <span class="name">squad</span>
     <span>.</span>
-    <span class="portfolio">page</span>
+    <span class="portfolio">baguette</span>
 
     <span class="dot dot1">.</span>
     <span class="dot dot2">.</span>
