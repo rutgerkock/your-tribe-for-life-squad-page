5b23b48872603862c55c47b8ee20f022eaa7a3ca
Anna-Kyra <anna-kyra.strik@hva.nl>
2024-09-16 16:03:56 +0200

    Witruimte + global css
    - witruimte nu weg
    - global.css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 </script>
  
 <!-- Only render if we have people in the data -->
-<section>
+<main>
     <div class="grid">
         {#if data.people}
         {#each data.people as person}
@@ -22,62 +22,58 @@
         <p>No data available</p>
         {/if}
     </div>
- 
-</section>
+</main>
 
 <style>
-    body{
-        margin: 0;
-    }
-section{
-    background-color: rgb(16, 36, 16);
-    max-width: 100%;
-    & .grid{
-        padding: 1em;
-        padding-top: 5em;
-        padding-bottom: 5em;
-        margin: 0 auto;
-        display: grid;
-        grid-template-columns: repeat(5, 1fr);
-        grid-template-rows: repeat(8, 1fr);
-        grid-column-gap: 32px;
-        grid-row-gap: 32px;
-        max-width: 1000px;
-    }
-    & article{
-        height: 220px;
-        padding: 32px;
-        border-radius: 1.5em;
-        padding: 1em;
-        background: url(card.png);
-        background-position: center;
-        background-size: 80%;
-        background-repeat: no-repeat;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        background-color: #ededed;
-        box-shadow: 5px 5px 10px rgb(9, 20, 9);
-        transform: scale(120%) rotate(-5deg) translateX(-10%) translateY(7%) ;
-            transition: ease-in 0.3s;
-        & img{
-            box-shadow: 2px 2px 4px rgba(131, 131, 131, 0.311);
-            border-radius: 100%;
-            object-fit: cover;
-        }
 
-        &:hover{
-            cursor: pointer;
-            
-            transform: scale(125%) rotate(3deg) translateX(-12%) translateY(-10%) ;
-            transition: ease-in 0.15s;
-            box-shadow: 5px 5px 10px rgb(29, 66, 29);
-            z-index: 5;
-            
+    main {
+        max-width: 100%;
+        & .grid{
+            padding: 1em;
+            padding-top: 5em;
+            padding-bottom: 5em;
+            margin: 0 auto;
+            display: grid;
+            grid-template-columns: repeat(5, 1fr);
+            grid-template-rows: repeat(8, 1fr);
+            grid-column-gap: 32px;
+            grid-row-gap: 32px;
+            max-width: 1000px;
         }
+        & article{
+            height: 220px;
+            padding: 32px;
+            border-radius: 1.5em;
+            padding: 1em;
+            background: url(card.png);
+            background-position: center;
+            background-size: 80%;
+            background-repeat: no-repeat;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            background-color: #ededed;
+            box-shadow: 5px 5px 10px rgb(9, 20, 9);
+            transform: scale(120%) rotate(-5deg) translateX(-10%) translateY(7%) ;
+                transition: ease-in 0.3s;
+            & img{
+                box-shadow: 2px 2px 4px rgba(131, 131, 131, 0.311);
+                border-radius: 100%;
+                object-fit: cover;
+            }
 
+            &:hover{
+                cursor: pointer;
+                
+                transform: scale(125%) rotate(3deg) translateX(-12%) translateY(-10%) ;
+                transition: ease-in 0.15s;
+                box-shadow: 5px 5px 10px rgb(29, 66, 29);
+                z-index: 5;
+                
+            }
+
+        }
     }
-}
 </style>
 
